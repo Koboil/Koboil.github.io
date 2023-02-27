06fe5d72c730f5095ea5791a2a56007e0530df83
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-02-27 12:46:16 +0100

    refactor: remove unused files & format html/css/js

--- Battery/battery.js
+++
@@ -1,2 +0,0 @@
-console.log("A script that return battery status of the phone");
-console.log("This is a hotfix for the battery");
--- horloge/chrono/chrono.js
+++ horloge/chrono/chrono.js
@@ -1,12 +1,12 @@
 /*
-*
-*
-*   Début de la
-*   Partie : 
-*   Chronomètre
-*
-*
-*/
+ *
+ *
+ *   Début de la
+ *   Partie :
+ *   Chronomètre
+ *
+ *
+ */
 
 let h1 = document.getElementsByTagName('h1')[0];
 let startB = document.getElementById('strt');
@@ -18,72 +18,72 @@ let hrs = 0;
 let t;
 
 //ici on gère le passage de seconde à minute et de minute à heure
-function tick(){
-    sec++;
-    if (sec >= 60) {
-        sec = 0;
-        min++;
-        if (min >= 60) {
-            min = 0;
-            hrs++;
-        }
-    }
+function tick() {
+	sec++;
+	if (sec >= 60) {
+		sec = 0;
+		min++;
+		if (min >= 60) {
+			min = 0;
+			hrs++;
+		}
+	}
 }
 
 // add représente notre fonction principale.
 function add() {
-    tick();
-    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) 
-        	 + ":" + (min > 9 ? min : "0" + min)
-       		 + ":" + (sec > 9 ? sec : "0" + sec);
-    timer();
+	tick();
+	h1.textContent =
+		(hrs > 9 ? hrs : '0' + hrs) +
+		':' +
+		(min > 9 ? min : '0' + min) +
+		':' +
+		(sec > 9 ? sec : '0' + sec);
+	timer();
 }
 
 //la fonction timer permet d'exécuter notre fonction principale (add) toutes les secondes
 function timer() {
-    t = setTimeout(add, 1000);
+	t = setTimeout(add, 1000);
 }
 
-
-
-
 startB.addEventListener('click', (event) => {
-    timer();
-    disableButton(startB);
-    enableButton(stopB);
-    enableButton(resetB);
+	timer();
+	disableButton(startB);
+	enableButton(stopB);
+	enableButton(resetB);
+});
 
-  });
+stopB.addEventListener('click', (event) => {
+	clearTimeout(t);
+	enableButton(startB);
+	disableButton(stopB);
+});
 
-stopB.addEventListener('click',(event) => {
-    clearTimeout(t);
-    enableButton(startB);
-    disableButton(stopB);
-  });
+resetB.addEventListener('click', (event) => {
+	h1.textContent = '00:00:00';
+	sec = 0;
+	min = 0;
+	hrs = 0;
+	clearTimeout(t);
+	enableButton(startB);
+	disableButton(resetB);
+	disableButton(stopB);
+});
 
-resetB.addEventListener('click',(event) => {
-    h1.textContent = "00:00:00";
-    sec = 0; min = 0; hrs = 0;
-    clearTimeout(t);
-    enableButton(startB);
-    disableButton(resetB);
-    disableButton(stopB);
-  });
+function disableButton(bouttonADisable) {
+	bouttonADisable.disabled = true;
+}
 
-  function disableButton(bouttonADisable) {
-    bouttonADisable.disabled = true; 
-  }
-
-  function enableButton(bouttonAEnable) {
-    bouttonAEnable.disabled = false; 
-  }
+function enableButton(bouttonAEnable) {
+	bouttonAEnable.disabled = false;
+}
 /*
-*
-*
-*   Fin de la
-*   Partie : 
-*   Chronomètre
-*
-*
-*/
-
+ *
+ *
+ *   Fin de la
+ *   Partie :
+ *   Chronomètre
+ *
+ *
+ */
